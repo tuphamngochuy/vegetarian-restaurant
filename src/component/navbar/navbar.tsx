409bb387199af6a00540cc07a3c7406d463c4abb
huytn0601 <huytn0601@gmail.com>
2022-07-25 00:48:17 +0700

    fix navbar

--- src/component/navbar/navbar.tsx
+++ src/component/navbar/navbar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState, useEffect} from "react";
 import {
     Link
 } from 'react-router-dom';
@@ -7,24 +7,41 @@ import './navbar.css';
 
 
 export default function Navbar() {
-    return (
-      <div className="nav-wrapper">
-        <div className="nav-name">
-          Vegeterian Place
-        </div>
-        <div className="nav-button-list">
-          <ul>
-            <li className="nav-button">
-              <Link to='/'>Home</Link>
-            </li>
-            <li className="nav-button">
-              <Link to='/menu'>Menu</Link>
-            </li>
-            <li className="nav-button">
-              <Link to='/reservation'>Reservation</Link>
-            </li>
-          </ul>
-        </div>
+  const [scrollYPos, setScrollYPos] = useState(0);
+
+  const handleScroll = () => {
+    const position = window.pageYOffset;
+    setScrollYPos(position);
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    }
+  }, []);
+
+  const wrapperName = `nav-wrapper ${scrollYPos < 10 ? 'nav-show' : 'nav-hide'}`;
+  
+  return (
+    <div className={wrapperName}>
+      <div className="nav-name">
+        Vegeterian Place
       </div>
-    )
+      <div className="nav-button-list">
+        <ul>
+          <li className="nav-button">
+            <Link to='/'>Home</Link>
+          </li>
+          <li className="nav-button">
+            <Link to='/menu'>Menu</Link>
+          </li>
+          <li className="nav-button">
+            <Link to='/reservation'>Reservation</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
